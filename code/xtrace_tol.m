2ba0b3586b39e5468b1f298c5a6be96c291af4d9
Ethan Epperly <epperlyethan@gmail.com>
2023-01-07 15:36:41 -0800

    Add adaptive XTrace implementation

---
+++ code/xtrace_tol.m
@@ -0,0 +1,15 @@
+function [t,err,m] = xtrace_tol(matvec, n, abstol, reltol, varargin)
+Y = zeros(n,0); Om = zeros(n,0); Z = zeros(n,0);
+err = Inf;
+if (reltol ~=0); t = Inf; else; t = 0; end
+while err >= abstol + reltol * t
+    % Randomized SVD    
+    [NewOm,improved] = generate_test_matrix(n,max(2,size(Y,2)), ...
+        'improved',varargin{:});
+    Y = [Y matvec(NewOm)]; Om = [Om NewOm];%#ok<AGROW> 
+    [Q,R] = qr(Y,0);
+    Z = [Z matvec(Q(:,size(Z,2)+1:end))]; %#ok<AGROW> 
+    [t,err] = xtrace_helper(Y, Om, Z, Q, R, improved);
+end
+m = 2*size(Z,2);
+end
