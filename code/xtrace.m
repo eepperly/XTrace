cfd3223516f6187ede06ce5afff4b7873c850a41
Ethan Epperly <epperlyethan@gmail.com>
2023-04-19 15:58:53 -0700

    Add timing to XTrace

--- code/xtrace.m
+++ code/xtrace.m
@@ -1,9 +1,30 @@
-function [t,err] = xtrace(A, m, varargin)
+function [t,err,time,processtime] = xtrace(A, m, varargin)
 [matvec,n] = process_matrix(A, varargin{:});
 m = floor(m/2);
+
+%% Generate test matrix
+tic;
 [Om,improved] = generate_test_matrix(n,m,'improved',varargin{:});
+processtime = toc;
+
+%% First round of matvecs
+tic;
 Y = matvec(Om);
+matvectime = toc;
+
+%% Orthonormalize
+tic;
 [Q,R] = qr(Y,0);
+processtime = processtime + toc;
+
+%% First round of matvecs
+tic;
 Z = matvec(Q);
+matvectime = matvectime + toc;
+
+%% Complete
+tic;
 [t,err] = xtrace_helper(Om,Z,Q,R,improved);
+processtime = processtime + toc;
+time = processtime + matvectime;
 end
