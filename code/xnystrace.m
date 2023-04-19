b0669d83cde9a984f531ffd1215fa7570a32f041
Ethan Epperly <epperlyethan@gmail.com>
2023-04-19 16:00:57 -0700

    Add timing to XNysTrace

--- code/xnystrace.m
+++ code/xnystrace.m
@@ -1,7 +1,20 @@
-function [t,err] = xnystrace(A, m, varargin)
+function [t,err,time,processtime] = xnystrace(A, m, varargin)
 [matvec,n] = process_matrix(A, varargin{:});
+
+%% Generate test matrix
+tic;
 [Om,improved] = generate_test_matrix(n,m,'improved',varargin{:});
+processtime = toc;
+
+%% Matrix-vector products
+tic;
 Y = matvec(Om);
+matvectime = toc;
+
+%% Finish computation
+tic;
 [t,err] = xnystrace_helper(Y, Om, improved);
+processtime = processtime + toc;
+time = processtime + matvectime;
 end
 
