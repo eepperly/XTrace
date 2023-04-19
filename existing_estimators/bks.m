b681505a88df5e32d14483c196ec53f8e17d162d
Ethan Epperly <epperlyethan@gmail.com>
2023-04-19 16:13:42 -0700

    Add timing to BKS and Diag++

--- existing_estimators/bks.m
+++ existing_estimators/bks.m
@@ -1,5 +1,8 @@
-function d = bks(A,m,varargin)
+function [d,err,time,processtime] = bks(A,m,varargin)
 [matvec,n] = process_matrix(A, varargin{:});
-Om = generate_test_matrix(n,m,'signs',varargin{:});
-d = diag_prod(Om',matvec(Om).') / m;
+tic; Om = generate_test_matrix(n,m,'signs',varargin{:}); processtime = toc;
+tic; Y = matvec(Om); matvectime = toc;
+tic; d = diag_prod(Om',Y.') / m;
+processtime = processtime + toc; time = processtime + matvectime;
+err = NaN(size(d));
 end
--- existing_estimators/diag_plusplus.m
+++ existing_estimators/diag_plusplus.m
@@ -1,15 +1,27 @@
-function d = diag_plusplus(A, m, varargin)
-% HUTCH_PLUSPLUS Hutch++ trace estimator
+function [d,err,time,processtime] = diag_plusplus(A, m, varargin)
+% DIAG_PLUSPLUS Diag++ trace estimator
 % Implementation is a modification of:
 %    https://github.com/RaphaelArkadyMeyerNYU/HutchPlusPlus/blob/main/simple/simple_hutchplusplus.m
 [matvec,n] = process_matrix(A, varargin{:});
 
+tic;
 S = generate_test_matrix(n,ceil(m/3),'signs',varargin{:});
 G = generate_test_matrix(n,floor(m/3),'signs',varargin{:});
+processtime = toc; 
 
-[Q,~] = qr(matvec(S),0);
-B = matvec(G - Q*(Q'*G));
+tic; Y = matvec(S); matvectime = toc;
+tic
+[Q,~] = qr(Y,0);
+Y = G - Q*(Q'*G);
+processtime = processtime + toc;
+tic;
+B = matvec(Y);
+matvectime = matvectime + toc;
 
+tic;
 d = diag_prod(Q',(Q'*matvec(Q))*Q') ...
     + sum(G .* (B - Q*(Q'*B))) / size(G,2);
+processtime = processtime + toc;
+err = NaN(size(d));
+time = processtime + matvectime;
 end
